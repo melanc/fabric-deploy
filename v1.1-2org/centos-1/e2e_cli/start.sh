247aaa7caa5dc72266101a904d753d25de32f863
wanghd <wanghedong@zsbatech.com>
2019-06-17 16:27:20 +0800

    update

--- v1.1-2org/centos-1/e2e_cli/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-docker-compose -f docker-compose-ca.yaml up -d
-
-sleep 2
-
-docker-compose -f docker-compose-orderer.yaml up -d
--- v1.1-2org/centos-5/e2e_cli/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-docker-compose -f docker-compose-ca.yaml up -d
-
-sleep 2
-
-docker-compose -f docker-compose-peer.yaml up -d
